b16809570b8ebaf8c4024fddb98becfa7d80bf39
Hajo Kleingeld <hajokleingeld@gmail.com>
2017-10-17 02:07:20 +0200

    Added Matlab only evolution

--- Analyzers/Evolution/RunEvolution.m
+++ Analyzers/Evolution/RunEvolution.m
@@ -9,18 +9,21 @@ clear all
 % T = scalefactor /10 (35 becomes T = 3.5)
 %
 %     n   d   m   z   T
-LB = [0   0   0   1   1 ]; %lower bound
-UB = [0   0   800 5   400]; %upper bound
+LB = [1   0   0   1   1 ]; %lower bound
+UB = [1   500 800 1   400]; %upper bound
 
 %tbh, no clue what this does, just copy pasted it form the web, it puts up
 %a screen which plots the progression of the algorithm
 options = optimoptions(@ga,'PlotFcn',{@gaplotbestf,@gaplotmaxconstr},'Display','iter');
 
 % Run on Car dataset
-[x,fval,exitflag,output,population,scores] = ga(@Evolution,5,[],[],[],[],LB,UB,@simple_constraint,[1,2,3,4,5],options)
+%[x,fval,exitflag,output,population,scores] = ga(@Evolution,5,[],[],[],[],LB,UB,@simple_constraint,[1,2,3,4,5],options)
 
 % Run on darkroom dataset
 %[x,fval,exitflag,output,population,scores] = ga(@Evolution_darkroom,5,[],[],[],[],LB,UB,@simple_constraint,[1,2,3,4,5],options)
 
 % Run on outside dataset
 %[x,fval,exitflag,output,population,scores] = ga(@Evolution_outside,5,[],[],[],[],LB,UB,@simple_constraint,[1,2,3,4,5],options)
+
+%Run with matlab only
+[x,fval,exitflag,output,population,scores] = ga(@Evolution_Matlab_Only,5,[],[],[],[],LB,UB,@simple_constraint,[1,2,3,4,5],options)
