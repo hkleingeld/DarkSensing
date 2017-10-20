a8ce8ab231aecd3616a63db95680fda80f2341b6
Hajo Kleingeld <hajokleingeld@gmail.com>
2017-10-20 14:51:26 +0200

    Updated filter in algorihm to be the same as the matlab implementaion.

--- Analyzers/Evolution/Algorithm.m
+++ Analyzers/Evolution/Algorithm.m
@@ -5,8 +5,11 @@ if(z > Z)
 end
 
 [SIZE dc] = size(x);
-les = zeros(SIZE,1);
-les(lesson:SIZE) = 1;
+lesTP = zeros(SIZE,1);
+lesTP(lesson:SIZE) = 1;
+
+lesFP = ~lesTP;
+lesFP(1:d+m+265) = 0;
 
 filter(1:Z) = 1/z;
 
@@ -14,8 +17,8 @@ X = movmean(x,[n-1 0]);
 avg = [zeros(d-1,1);movmean(X,[m-1 0])];
 std = [zeros(d-1,1);movstd(X,[m-1 0])];
 
-TP = (conv(filter,les .* ((X(1:SIZE) < avg(1:SIZE)-T*std(1:SIZE)) | (X(1:SIZE) > avg(1:SIZE)+T*std(1:SIZE)))) >= 1);
-FP = (conv(filter,~les .* ((X(1:SIZE) < avg(1:SIZE)-T*std(1:SIZE)) | (X(1:SIZE) > avg(1:SIZE)+T*std(1:SIZE)))) >= 1);
+TP = (conv(filter,lesTP .* ((X(1:SIZE) < avg(1:SIZE)-T*std(1:SIZE)) | (X(1:SIZE) > avg(1:SIZE)+T*std(1:SIZE)))) >= 1);
+FP = (conv(filter,lesFP .* ((X(1:SIZE) < avg(1:SIZE)-T*std(1:SIZE)) | (X(1:SIZE) > avg(1:SIZE)+T*std(1:SIZE)))) >= 1);
 
 result = [any(TP(n+m+d+100:SIZE)) any(FP(n+m+d+100:SIZE))];
 
